811c8250781905343b4fec6d1853e26b04680b0c
Ali <0ali0afshar0@gmail.com>
2025-04-06 14:40:28 +0330

    Add Extractor class and extract command from input

---
+++ Extractor.java
@@ -0,0 +1,16 @@
+package ir.ac.kntu;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Extractor {
+    public static void extractCommand(InputData inputData) throws IllegalArgumentException {
+        Pattern pattern = Pattern.compile("^\\s*([a-zA-Z0-9_]+)");
+        Matcher matcher = pattern.matcher(inputData.input);
+        if (matcher.find())
+            inputData.command = matcher.group(1).trim();
+        else
+            throw new IllegalArgumentException("Error: Invalid command format");
+    }
+
+}
