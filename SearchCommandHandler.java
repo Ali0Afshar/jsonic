baef0b00a2ee84ec8b9bdbb56eada0286e056aa5
Ali <0ali0afshar0@gmail.com>
2025-04-15 19:09:41 +0330

    feat: add SearchCommandHandler and print result

---
+++ SearchCommandHandler.java
@@ -0,0 +1,39 @@
+package ir.ac.kntu;
+
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SearchCommandHandler implements CommandHandler {
+    public void handle(InputData inputData) throws IllegalArgumentException {
+        if (!isValidInput(inputData.input))
+            throw new IllegalArgumentException("Error: Invalid input.");
+
+        Extractor.extractTypeName(inputData);
+        checkTypename(inputData.typeName);
+        
+        if (!Extractor.extractConditions(inputData))
+            inputData.conditions = null;
+        
+        Database db = Database.getInstance();
+        for (HashMap<String, Object> data : db.getFilteredData(inputData.typeName, inputData.conditions)) {
+            System.out.println(data);
+            return;
+        }
+        // No data matches these conditions.
+    }
+
+    private boolean isValidInput(String input) {
+        Pattern pattern = Pattern.compile("^\\s*[A-Za-z0-9_]+\\s+[A-Za-z0-9_]+(\\s*\\(.*\\))?\\s*$");
+        Matcher matcher = pattern.matcher(input);
+        if (matcher.find())
+            return true;
+        return false;
+    }
+
+    private void checkTypename(String type) throws IllegalArgumentException {
+        Database db = Database.getInstance();
+        if (db.getTypeFormatByType(type) == null)
+            throw new IllegalArgumentException("Error: Type '" + type + "' not exist.");
+    }
+}
